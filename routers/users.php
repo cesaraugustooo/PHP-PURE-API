279227eb7ede2aa69be144d0c629068e33349adb
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-01-04 23:08:40 -0300

    Adicionei um get com id

--- routers/users.php
+++ routers/users.php
@@ -4,9 +4,20 @@ require_once 'controlers/users.php';
 
 switch($getHTTP){
     case 'GET':
-        UserControler::getAllUsers();
+        if (preg_match('#(\d+)#', $rota, $array )){
+            $id = $array[0];
+            UserControler::get($id);
+        }else{  
+            UserControler::getAllUsers();
+        }   
         break;
     case 'POST':
         UserControler::post();
         break;
+    case 'DELETE':
+        if(preg_match('#(\d+)#', $rota , $array)){
+            $id = $array[0];
+            UserControler::delete($id);
+        }
+
 }
