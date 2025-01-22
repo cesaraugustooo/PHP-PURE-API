6c932bc72167bff0a4e6c0c78879e9b2080e6c0d
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-01-21 23:35:36 -0300

    Tratamendo de erros

--- models/users.php
+++ models/users.php
@@ -8,9 +8,9 @@ class User{
     public static function getAll(){
         $db = Database::connection();
         $sql = $db->prepare("SELECT usuarios.id_usuarios,usuarios.nif,
-        usuarios.nome_usuario,usuarios.email_usuario,usuarios.foto_usuario FROM usuarios ");
+        usuarios.nome_usuario,usuarios.email_usuario,usuarios.foto_usuario FROM usuarios WHERE ativo = true ");
         $sql->execute();
-
+    
         return $sql->fetchAll(PDO::FETCH_ASSOC,);
     }
     public static function post($dados){
@@ -25,6 +25,12 @@ class User{
         $sql ->bindValue(':ativo',$dados['ativo']);
         $sql->execute();
 
+        if($sql->rowCount() > 0){
+            header('Content-Type: application/json');
+            echo json_encode(["status" => "usuario cadastrado com sucesso!"]);
+        }else{
+            json_error_response();
+        }
 
     }
     public static function get($id){
@@ -37,10 +43,16 @@ class User{
     }
     public static function delete($id){
         $db = Database::connection();
-        $sql = $db->prepare("DELETE FROM usuarios WHERE id_usuarios = :id");
+        $sql = $db->prepare("UPDATE usuarios SET ativo = 0 WHERE id_usuarios = :id");
         $sql->bindValue(':id',$id, PDO::PARAM_INT);
         $sql->execute();
 
+        if($sql->rowCount() > 0){
+            header('Content-Type: application/json');
+            echo json_encode(["status" => "usuario desativado com sucesso!"]);       
+        }else{
+            json_error_response();
+        }
   
     }
 }
