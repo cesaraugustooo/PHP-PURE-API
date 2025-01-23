f4d9de52425455e3e2d81d61de432fedfce5d096
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-01-22 23:16:49 -0300

    add delete

--- routers/categorias.php
+++ routers/categorias.php
@@ -18,5 +18,10 @@ switch($getHTTP){
     case 'POST':
         categoriasController::post();
         break;
+    case 'PATCH':
+        if(preg_match('#(\d+)#', $rota , $array)){
+            categoriasController::delete($array[0]);
+            break;
+        }
 }
 
