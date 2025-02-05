9d950db766509818d8e18740247fae8473ee3965
cesaraugustooo <164028550+cesaraugustooo@users.noreply.github.com>
2025-02-05 13:58:05 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -7,6 +7,7 @@ class Database
 
     public static function connection()
     {
+        #Mude os dados referente ao se banco de dados#
         self::$connection = new PDO('mysql:host=localhost;dbname=sistemadecontagens', 'root' , '');
 
         return self::$connection;
