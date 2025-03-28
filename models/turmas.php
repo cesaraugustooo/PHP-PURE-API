484739da72c6ce13a51eae2ad857dd824711e770
CESAR AUGUSTO ESQUIÇACTO FERREIRA <cesar.ferreira@portalsesisp.org.br>
2025-03-28 10:52:59 -0300

    api v2

--- models/turmas.php
+++ models/turmas.php
@@ -1,27 +1,28 @@
 <?php
 
-require_once 'config/database.php';
+require_once 'c://xampp/htdocs/PHP-PURE-API/config/database.php';
+require_once 'json_requests/response.php';
 
 class Turmas
 {
     public static function getAll(){
         $db = Database::connection();
-        $sql = $db->prepare("SELECT turmas.id_turma,turmas.ano_turma,turmas.nome_turma,turmas.ativo,categorias.nome_categoria FROM turmas LEFT JOIN categorias ON turmas.categorias_id_categoria = categorias.id_categoria WHERE turmas.ativo = 1");
+        $sql = $db->prepare("SELECT * FROM turmas");
         $sql->execute();
         return $sql->fetchAll(PDO::FETCH_ASSOC);
     }
     public static function post($turma){
         $db = Database::connection();
-        $sql = $db->prepare("INSERT INTO turmas VALUES (null,:ano,:nome,:ativo,:categoria)");
-        $sql->bindValue(':ano',$turma['ano_turma']);
+        $sql = $db->prepare("INSERT INTO turmas VALUES (null,:nome,:categoria)");
         $sql->bindValue(':nome',$turma['nome_turma']);
-        $sql->bindValue(':ativo',$turma['ativo']);
-        $sql->bindValue('categoria',$turma['categorias_id_categoria']);
+        $sql->bindValue(':categoria',$turma['categorias_id_categoria']);
         $sql->execute();
+
+        json_success_response($sql);
     }
     public static function get($id){
         $db =Database::connection();
-        $sql = $db->prepare("SELECT * FROM turmas WHERE id_turma = :id AND ativo = 1 ");
+        $sql = $db->prepare("SELECT * FROM turmas WHERE id_turma = :id");
         $sql->bindValue(':id',$id,PDO::PARAM_INT);
         $sql->execute();
 
@@ -29,7 +30,7 @@ class Turmas
     }
     public static function delete($id){
         $db = Database::connection();
-        $sql = $db->prepare("UPDATE  turmas SET ativo = 0 WHERE id_turma = :id");
+        $sql = $db->prepare("DELETE FROM turmas WHERE id_turma = :id");
         $sql->bindValue(':id',$id,PDO::PARAM_INT);
         $sql->execute();
 
@@ -44,10 +45,8 @@ class Turmas
     }
     public static function update($id,$turma){
         $db = Database::connection();
-        $sql = $db->prepare("UPDATE turmas SET ano_turma = :ano, nome_turma = :nome, categorias_id_categoria = :ctegoria  WHERE id_turma = :id");
-        $sql->bindValue(':ano',$turma['ano_turma']);
+        $sql = $db->prepare("UPDATE turmas SET  nome_turma = :nome, categorias_id_categoria = :categoria  WHERE id_turma = :id");
         $sql->bindValue(':nome',$turma['nome_turma']);
-        $sql->bindValue(':ativo',$turma['ativo']);
         $sql->bindValue('categoria',$turma['categorias_id_categoria']);
         $sql->bindValue(':id',$id,PDO::PARAM_INT);
         $sql->execute();
