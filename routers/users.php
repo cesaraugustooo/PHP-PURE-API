6fb20c16298f31decf76bc59565f1c82279c84eb
CESAR AUGUSTO ESQUIÇACTO FERREIRA <cesar.ferreira@portalsesisp.org.br>
2025-03-28 10:53:28 -0300

    api v2

--- routers/users.php
+++ routers/users.php
@@ -14,7 +14,7 @@ switch($getHTTP){
     case 'POST':
         UserControler::post();
         break;
-    case 'PATCH':
+    case 'DELETE':
         if(preg_match('#(\d+)#', $rota , $array)){
             $id = $array[0];
             UserControler::delete($id);
